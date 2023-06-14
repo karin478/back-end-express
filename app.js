c7cc538e2ff492845d484bb0ff1b22f1a8edf695
Karin <79677735+karin478@users.noreply.github.com>
2023-06-14 01:30:19 +0000

    Update app.js and auth.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth"); // Import the auth route
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter); // Use the auth route
 
 app.get("/test", (req, res) => {
   res.send({ message: "Test route is working!" });
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/test", (req, res) => {
+router.get("/test", (req, res) => {
   res.send({ message: "Test POST route is working!", data: req.body });
 });
 
